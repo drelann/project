0acf33f5746d0641844c07e78833a669a12df2d7
drelann <nastyahailey@yandex.ru>
2020-03-21 18:40:27 +0500

    task 1 up

--- api/routes/index.js
+++ api/routes/index.js
@@ -55,43 +55,43 @@ const NUMBERS = [
 router.get("/number/:number", function(request, response) {
   let length = +request.params["number"] >= 0 ? request.params["number"].length : request.params["number"].length - 1;
   let number = Math.abs(+request.params["number"]);
-  let data = +value >= 0 ? "" : "минус ";
+  let data = +request.params["number"] >= 0 ? "" : "минус ";
 
   switch (length) {
     case 1:
     case 2: {
-      if (number < 20) data += NUMBERS_ARRAY[0][number];
+      if (number < 20) data += NUMBERS[0][number];
       else {
         let firstDigit = Math.trunc(number / 10);
         let secondDigit = number % 10;
-        data = NUMBERS_ARRAY[1][firstDigit];
-        if (secondDigit !== 0) data += " " + NUMBERS_ARRAY[0][secondDigit];
+        data = NUMBERS[1][firstDigit];
+        if (secondDigit !== 0) data += " " + NUMBERS[0][secondDigit];
       }
       break;
     }
     case 3: {
       let firstDigit = Math.trunc(number / 100);
       let secondDigit = number % 100;
-      data += NUMBERS_ARRAY[2][firstDigit];
+      data += NUMBERS[2][firstDigit];
 
       if (secondDigit !== 0) {
-        if (secondDigit < 20) data += " " + NUMBERS_ARRAY[0][secondDigit];
+        if (secondDigit < 20) data += " " + NUMBERS[0][secondDigit];
         else {
           let digit1 = Math.trunc(secondDigit / 10);
           let digit2 = secondDigit % 10;
-          data += " " + NUMBERS_ARRAY[1][digit1];
-          if (digit2 !== 0) data += " " + NUMBERS_ARRAY[0][digit2];
+          data += " " + NUMBERS[1][digit1];
+          if (digit2 !== 0) data += " " + NUMBERS[0][digit2];
         }
       }
       break;
     }
     default: {
-      data = "такие числа писать не умею";
+      data = "введите трехзначное число";
       break;
     }
   }
 
-  response.render('index', { title: data });
+  response.render('index', { data: data });
 });
 
 
